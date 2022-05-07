8299a756cbb824f5e336f8af64691c8d87674401
loictossou2004 <loic.tossou@epitech.eu>
2022-05-07 18:56:19 +0100

    New design

---
+++ core/script.js
@@ -0,0 +1,94 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDtQcMZIgc7ZyxMUCEeF-nXfi3_-PfrJkg",
+    authDomain: "my-home-d35d6.firebaseapp.com",
+    databaseURL: "https://my-home-d35d6-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "my-home-d35d6",
+    storageBucket: "my-home-d35d6.appspot.com",
+    messagingSenderId: "908101030608",
+    appId: "1:908101030608:web:841352982c506f71b904d5",
+    measurementId: "G-JM1W2P70QT"
+};
+firebase.initializeApp(firebaseConfig);
+
+$(document).ready(function () {
+    var database = firebase.database();
+	var Led1Status;
+	var Led2Status;
+	var Led3Status;
+	var Led4Status;
+	database.ref().on("value", function(snap){
+		Led1Status = snap.val().Led1Status;
+		Led2Status = snap.val().Led2Status;
+		Led3Status = snap.val().Led3Status;
+		Led4Status = snap.val().Led4Status;
+		if(Led1Status == "1"){
+			document.getElementById("unact").style.display = "none";
+			document.getElementById("act").style.display = "block";
+		} else {
+			document.getElementById("unact").style.display = "block";
+			document.getElementById("act").style.display = "none";
+		}
+		if(Led2Status == "1"){
+			document.getElementById("unact1").style.display = "none";
+			document.getElementById("act1").style.display = "block";
+		} else {
+			document.getElementById("unact1").style.display = "block";
+			document.getElementById("act1").style.display = "none";
+		}
+		if(Led3Status == "1"){
+			document.getElementById("unact2").style.display = "none";
+			document.getElementById("act2").style.display = "block";
+		} else {
+			document.getElementById("unact2").style.display = "block";
+			document.getElementById("act2").style.display = "none";
+		}
+		if(Led4Status == "1"){
+			document.getElementById("unact3").style.display = "none";
+			document.getElementById("act3").style.display = "block";
+		} else {
+			document.getElementById("unact3").style.display = "block";
+			document.getElementById("act3").style.display = "none";
+		}
+	});
+
+	$(".toggle-btn").click(function(){
+		var firebaseRef = firebase.database().ref().child("Led1Status");
+		if(Led1Status == "1"){
+			firebaseRef.set("0");
+			Led1Status = "0";
+		} else {
+			firebaseRef.set("1");
+			Led1Status = "1";
+		}
+	})
+	$(".toggle-btn1").click(function(){
+		var firebaseRef = firebase.database().ref().child("Led2Status");
+		if(Led2Status == "1"){
+			firebaseRef.set("0");
+			Led2Status = "0";
+		} else {
+			firebaseRef.set("1");
+			Led2Status = "1";
+		}
+	})
+	$(".toggle-btn2").click(function(){
+		var firebaseRef = firebase.database().ref().child("Led3Status");
+		if(Led3Status == "1"){
+			firebaseRef.set("0");
+			Led3Status = "0";
+		} else {
+			firebaseRef.set("1");
+			Led3Status = "1";
+		}
+	})
+	$(".toggle-btn3").click(function(){
+		var firebaseRef = firebase.database().ref().child("Led4Status");
+		if(Led4Status == "1"){
+			firebaseRef.set("0");
+			Led4Status = "0";
+		} else {
+			firebaseRef.set("1");
+			Led4Status = "1";
+		}
+	})
+});
